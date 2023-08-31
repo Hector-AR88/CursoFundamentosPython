9b59430f9b8103f08a264671ff4a575443e335c3
Hector Alpuche Rodriguez <halpuche@cocobongo.com>
2023-08-30 23:04:59 -0500

    creando repositorio

---
+++ 22_for.py
@@ -0,0 +1,23 @@
+# for element in range(1,21):
+#     print(element)
+
+# my_list = [23,45,67,89,43]
+# for element in my_list:
+#     print(element)
+
+# my_tuple = ('nico','julio','hector')
+# for element in my_tuple:
+#     print(element)
+
+
+# product = {
+#     'name': 'camisa',
+#     'price': 100,
+#     'stock': 13
+# }
+# for key in product:
+#     print(key, '=>', product[key])
+# for key, value in product.items():
+#     print(key, '=>', value)
+
+
