2e75b94db9010c2a93678d02c31b8729c2c43427
Hector Alpuche Rodriguez <halpuche@cocobongo.com>
2023-08-31 00:18:55 -0500

    ejercicios 01 y 02

---
+++ 01_print.py
@@ -0,0 +1,18 @@
+# ejemplos de impresion por consola
+print('Hola este es el archivo 01')
+# print('Hola soy Hector y tengo 34 anios')
+
+
+# ejemplos de impresion de operaciones
+print(12+5)
+print(10-5)
+print(2 * 3)
+print(8/2)
+
+
+'''
+comeentario
+de
+varias
+lineas
+'''
---
+++ 02_vars.py
@@ -0,0 +1,23 @@
+print('Hola vars')
+
+# # esto es una variable con una cadena
+# my_name = 'Hector'
+# print(my_name)
+
+# # esto es una variable con un numero
+# my_age = 34
+# print(my_age)
+
+# # esto es un cambio de valor para una variable
+# my_name = 'Hector AR'
+# print(my_name)
+
+# # impresion de cadena concatenada
+# print('my nombre es ', my_name,' y mi edad es ', my_age)
+
+# input
+
+my_name = input('escribe tu nombre: ')
+my_age = input('escribe tu edad: ')
+
+print('Hola ', my_name, 'de ', my_age,' anios')
